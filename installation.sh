004bd7c8d1c9c42e9eaa2b8dcc5e405bfdc59df2
Gaba <chidiac200@gmail.com>
2021-05-18 19:29:49 +0200

    VERSION 1.1.0

--- installation.sh
+++ installation.sh
@@ -6,15 +6,15 @@ echo "Faites un choix"
 echo "1 - Installation de Prometheus"
 echo "2 - Installation de Node Exporteur"
 
-
-node_exporter = 1.1.2
-
 echo -n "Entrer une Valeur: "
+
 read ACTION
 
 case $ACTION in
 
 	1)
+		echo -n 1
+		
 		wget https://github.com/prometheus/prometheus/releases/download/v2.27.0/prometheus-2.27.0.linux-amd64.tar.gz
 
 		echo "--------SOURCE RECUPEREE------------"
@@ -24,19 +24,27 @@ case $ACTION in
 		echo "-------SOURCE DECOMPRESSEE----------"
 	
 		rm -f prometheus-*.tar.gz
-
+		
+		;;
 
 	2)
 
-		wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-$node_exporter.linux-amd64.tar.gz
+		wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+	
+		echo -n 2
 		
 		echo "--------SOURCE RECUPEREE------------"
 
-                tar xvfz node_exporter-$node_exporter-amd64.tar.gz
+                tar xvfz node_exporter-*.*-amd64.tar.gz
 
                 echo "-------SOURCE DECOMPRESSEE----------"
 
                 rm -f node_exporter-*.tar.gz
 
+		;;
+
 	*)
-		echo "IMPOSSIBLE VOTRE CHOIX"
+		echo -n "IMPOSSIBLE VOTRE CHOIX"
+		
+		;;
+esac

b6cb836391a32640ca5902f04320638320870532
Gaba <chidiac200@gmail.com>
2021-05-18 13:18:59 +0200

    Fichier d'installation valide

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+wget https://github.com/prometheus/prometheus/releases/download/v2.27.0/prometheus-2.27.0.linux-amd64.tar.gz
+
+echo "--------SOURCE RECUPEREE------------"
+
+tar xvfz prometheus-*.tar.gz
+
+echo "-------SOURCE DECOMPRESSEE----------"
+
+rm -f prometheus-*.tar.gz
+
+
+
